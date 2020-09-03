b7f0dab47c1d37ac0f3b025e72207480c764e410
hx76 <947769582@qq.com>
2020-09-03 19:08:15 +0800

    Initial commit

---
+++ laundry-consumer-index80/src/main/java/com/athx76/LoginMain80.java
@@ -0,0 +1,11 @@
+package com.athx76;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(LoginMain80.class,args);
+    }
+}
